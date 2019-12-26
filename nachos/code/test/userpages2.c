16bc5be6016d82879209ea7d7d8f8fa2a7edd406
Alexis Cataldi <acataldi@emi.u-bordeaux1.fr>
2019-12-26 15:10:30 +0100

    fichier 2.4

---
+++ nachos/code/test/userpages2.c
@@ -0,0 +1,29 @@
+#include "syscall.h"
+
+/*
+  pour lancer :
+  ./userprog/nachos -rs 1234 -x ./test/userpages2
+  comportement attendu :
+  affiche 12 fois le caractère 2
+
+  *note : programme de test pour la partie II du TD3
+*/
+
+void putchar_thread(void *c)
+{
+    // cette convertion est utiliser pour éviter des "warnings" sur les types
+    PutChar((char)(*(char *)c));
+    ThreadExit();
+}
+
+
+int main()
+{
+    for(int i = 0; i<= 12 ; i++){
+	    char c = '2';
+	    ThreadCreate(putchar_thread, &c);
+    }
+
+    ThreadExit();
+    Exit(0); // ce Exit() n'est pas atteint à cause du ThreadExit() juste avant
+}
